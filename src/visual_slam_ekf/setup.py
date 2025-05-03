7a78fd346be3e4211a8b8fd78e7f0b7dfbc36c16
skarnati20 <sai_siddhi_karnati@brown.edu>
2025-05-03 17:14:54 -0400

    Initial project setup

---
+++ src/visual_slam_ekf/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'visual_slam_ekf'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='skarnati20',
+    maintainer_email='sai_siddhi_karnati@brown.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
